68d4e1d42a0d8e1ae5f138e1eea05d1838be31f0
jdrabot <jdrabot@hotmail.com>
2021-03-06 22:56:55 -0500

    fixed some naming conventions in js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,11 @@
-var form = document.getElementById();
 var apiKey = "1cfba6dc832de74b8d22e5f7cc8acfd1";
-var inputValue = input.value;
+var inputCity = document.getElementById("city-name");
 var requestUrl = "https://api.openweathermap.org/data/2.5/weather?q=${inputVal}&appid=$1cfba6dc832de74b8d22e5f7cc8acfd1&units=metric`;";
 
-form.addEventListener("submit", function(event) {
-   event.preventDefault() 
-})
+form.addEventListener("submit", function (event) {
+    event.preventDefault();
+    var inputValue = inputCity.value;
+});
 
 function getApi(requestUrl) {
     fetch(requestUrl)
