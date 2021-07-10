dfedf5009b79c5436a3a4cf38f3bfa118355b0d5
jdrabot <jdrabot@hotmail.com>
2021-07-10 19:30:08 -0400

    starting the js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,11 @@
 var apiKey = "1cfba6dc832de74b8d22e5f7cc8acfd1";
 var inputCity = document.getElementById("city-name");
 var requestUrl = "https://api.openweathermap.org/data/2.5/weather?q=${inputVal}&appid=$1cfba6dc832de74b8d22e5f7cc8acfd1&units=metric`;";
+var cityName = document.getElementByClassName("city");
+var cityTemp = documnet.getElementByClassName("temp");
+var cityHumidity = document.getElementByClassName("humidity");
+var cityWindSpeed = document.getElementByClassName("windspeed");
+var cityUVIndex = document.getElementByClassName("uvindex");
 
 form.addEventListener("submit", function (event) {
     event.preventDefault();
